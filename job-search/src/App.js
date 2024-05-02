2a3349a44f43678e3cafcd6562962898d478214a
shreyash-lana <shreyash.siddharth@lana.health>
2024-05-02 15:43:13 +0530

    API Integrated and Responsive Layout Built

--- job-search/src/App.js
+++ job-search/src/App.js
@@ -1,12 +1,55 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './Components/Card/card';
 
 
 function App() {
+  const [jobListFromApi , setJobListFromApi] = useState([]);
+  const [offset , setOffset] = useState(0);
+  const [loading , setLoading] = useState(false);
+  const [hasMore , setHasMore] = useState(true);
+  const fetchData = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ limit: 9, offset: offset })
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      if ( !data || data?.jdList.length === 0) {
+        setHasMore(false);
+      } else {
+        setJobListFromApi(prevItems => [...prevItems, ...data?.jdList]);
+        setOffset(prevOffset => prevOffset + 9);
+      }
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+
+    setLoading(false);
+  };
+  useEffect(()=>{
+    fetchData();
+  },[])
+  useEffect(()=>{
+    console.log(jobListFromApi);
+  },[jobListFromApi])
+
   return (
-    <>
-    <div style={{display: 'flex' , justifyContent:"flex-start"}}>
+    <div style={{padding:"2rem"}} >
+    <div style={{display: 'flex' , justifyContent:"flex-start", flexFlow:"wrap"}}>
     <select
     style={{
       padding : '0.56rem',
@@ -101,8 +144,14 @@ function App() {
       
   
     </div>
-    <Card />
-    </>
+    
+    <div style={{width : "100%" , display:"flex", flexFlow:"wrap",gap:"6rem" , justifyContent:"flex-start"}}>
+      {jobListFromApi.map((item , index) =>(
+        <Card key={index} data={item} />
+      ))}
+    </div>
+    
+    </div>
   );
 }
 
--- job-search/src/Components/Card/card.jsx
+++ job-search/src/Components/Card/card.jsx
@@ -1,7 +1,46 @@
-import React from "react";
-import '../Card/card.css';
+import React, { useEffect, useState } from "react";
+import "../Card/card.css";
 
-function Card() {
+function Card({data}) {
+  const [aboutCompany, setAboutCompany] = useState("");
+  const [jobRole , setJobRole] = useState("");
+  const [jobLocation , setJobLocation] = useState("");
+  const [minExp , setMinExp] = useState("");
+  const [minJdSalary , setMinJdSalary] = useState("");
+  const [maxJdSalary , setMaxJdSalary] = useState("");
+  const [currencyCode , setCurrencyCode] = useState("")
+
+  const [isDescExpanded, setIsDescExpanded] = useState(false);
+
+  useEffect(() => {
+    setJobRole(data?.jobRole);
+    setJobLocation(data?.location);
+    setMinExp(data?.minExp);
+    setMinJdSalary(data?.minJdSalary);
+    setMaxJdSalary(data?.maxJdSalary)
+    setAboutCompany(data?.jobDetailsFromCompany);
+    setCurrencyCode(data?.salaryCurrencyCode);
+  }, []);
+  const getFirstFewWords = (text) => {
+    const words = text.split(/\s+/);
+    return words.slice(0, 35).join(" ") + "...";
+  };
+  const salaryText = 
+    (currencyCode === "USD")?
+        (minJdSalary)?
+             `Estimated Salary: ${minJdSalary}K -  ${maxJdSalary}K USD ✅ `
+        :
+           `Estimated Salary: Upto ${maxJdSalary}K USD ✅ `
+        
+    :
+        (minJdSalary)?
+             `Estimated Salary: Rs${minJdSalary} -  ${maxJdSalary} Lakh ✅ `
+        :
+             `Estimated Salary: Upto Rs${maxJdSalary} Lakh ✅ `
+        
+    
+  
+
   return (
     <div
       style={{
@@ -10,11 +49,11 @@ function Card() {
         height: "100%",
         maxWidth: "360px",
         padding: "1rem",
-        margin: "1rem",
+        margin: "3rem",
         opacity: "87.0%",
         boxShadow: "rgba(0, 0, 0, 0.25) 0px 1px 4px 0px",
         transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-        overflow:"hidden",
+        overflow: "hidden",
       }}
     >
       <div
@@ -31,69 +70,122 @@ function Card() {
       >
         ⏳ Posted 13 days ago
       </div>
-      <div style={{display : "flex", marginTop:"1rem", gap:"0.5rem" }}>
-        <img src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713598325603_7ico7.jpg" alt="logo" style={{width:"25px", height:"2.5rem"}} />
+      <div style={{ display: "flex", marginTop: "1rem", gap: "0.5rem" }}>
+        <img
+          src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713598325603_7ico7.jpg"
+          alt="logo"
+          style={{ width: "25px", height: "2.5rem" }}
+        />
         <div>
-            <h3 style={{margin:"0",
-                fontSize:"13px",
-                fontWeight:"600px",
-                letterSpacing:"1px",
-                marginBottom:"3px",
-                color:"#8b8b8b",
-            }} className="companyName">fampay</h3>
-            <h2 style={{margin:"0", 
-                fontSize:"14px",
-                lineHeight:"1.5",
-                fontWeight:"400px",
-            }}>Backend Engineer</h2>
-            <p style={{margin:"0", fontSize:"11px" , fontWeight:"bold", marginTop:"5px"}}>Bangalore</p>
+          <h3
+            style={{
+              margin: "0",
+              fontSize: "13px",
+              fontWeight: "600px",
+              letterSpacing: "1px",
+              marginBottom: "3px",
+              color: "#8b8b8b",
+            }}
+            className="companyName"
+          >
+            Sample Company
+          </h3>
+          <h2
+            style={{
+              margin: "0",
+              fontSize: "14px",
+              lineHeight: "1.5",
+              fontWeight: "400px",
+            }}
+          >
+            {jobRole}
+          </h2>
+          <p
+            style={{
+              margin: "0",
+              fontSize: "11px",
+              fontWeight: "bold",
+              marginTop: "5px",
+            }}
+          >
+            {jobLocation}
+          </p>
         </div>
       </div>
-      <div style={{fontWeight:"400", padding:"8px 0" , fontSize:"14px" , color:"rgb(77, 89, 106)"}}>
-      Estimated Salary: ₹18 - 35 LPA ✅
+      <div
+        style={{
+          fontWeight: "400",
+          padding: "8px 0",
+          fontSize: "14px",
+          color: "rgb(77, 89, 106)",
+        }}
+      >
+        {salaryText}
       </div>
       <div>
-        <p style={{marginBottom:"0.2rem" , marginTop:"0.3rem"}}>About Company:</p>
-        <p style={{margin:"0"}}>
-            <strong>
-            About us
-                </strong></p>
-      </div>
-        <p style={{fontSize:"14px" , fontWeight:"300", marginTop:"0", whiteSpace:"pre-wrap"}}>
-        FamPay is building India’s first neo-bank
-      exclusively teens. FamPay helps teens make their own online and offline
-      payments through UPI, FamPay App and FamCard. Our aim is to make banking
-      cool for teens and to help them learn the value of money, savings and
-      spending wisely. We are on a mission to raise a new, financially aware
-      generation, and drive 250 Million+ Indian teenagers to kicksta
+        <p style={{ marginBottom: "0.2rem", marginTop: "0.3rem" }}>
+          About Company:
         </p>
-    <div style={{backgroundColor:"#FFFFFF"}}>
-    <h3 style={{
-                fontSize:"13px",
-                fontWeight:"600px",
-                letterSpacing:"1px",
-                marginBottom:"0",
-                color:"#8b8b8b",
-            }} className="companyName">Minimum Experience</h3>
-        <p style={{margin:"0"}}>2 years</p>
-    <div style={{
-        margin:"0.5rem",
-        fontSize:"16px",
-        lineHeight:"1.75",
-        textAlign:"center",
-        padding:"8px 18px",
-        borderRadius:"8px",
-        fontWeight:"400",
-        backgroundColor:"#55efc4",
-        fill:"#000000",
-        cursor:"pointer",
-        userSelect:"none",
-        textDecoration:"none",
-      }}>
-        ⚡ Easy Apply
+        <p style={{ margin: "0" }}>
+          <strong>About us</strong>
+        </p>
       </div>
-    </div>
-    
+      <p
+        style={{
+          fontSize: "14px",
+          fontWeight: "300",
+          marginTop: "0",
+          whiteSpace: "pre-wrap",
+        }}
+      >
+        {isDescExpanded ? aboutCompany : getFirstFewWords(aboutCompany)}
+      </p>
+      <div style={{ display: "flex", justifyContent: "center", color: "blue" }}>
+        <p
+          style={{
+            cursor: "pointer",
+            margin: "0.3rem",
+            fontWeight: "300",
+            fontSize: "14px",
+          }}
+          onClick={() => setIsDescExpanded(!isDescExpanded)}
+        >
+          {isDescExpanded ? "Hide" : "View Job Description"}
+        </p>
+      </div>
+      <div style={{ backgroundColor: "#FFFFFF" }}>
+        <h3
+          style={{
+            fontSize: "13px",
+            fontWeight: "600px",
+            letterSpacing: "1px",
+            marginBottom: "0",
+            color: "#8b8b8b",
+          }}
+          className="companyName"
+        >
+          Minimum Experience
+        </h3>
+        <p style={{ margin: "0" }}>{minExp ? `${minExp} Years` : `Not Mentioned`}</p>
+        <div
+          style={{
+            margin: "0.5rem",
+            fontSize: "16px",
+            lineHeight: "1.75",
+            textAlign: "center",
+            padding: "8px 18px",
+            borderRadius: "8px",
+            fontWeight: "400",
+            backgroundColor: "#55efc4",
+            fill: "#000000",
+            cursor: "pointer",
+            userSelect: "none",
+            textDecoration: "none",
+          }}
+        >
+          ⚡ Easy Apply
+        </div>
+      </div>
     </div>
   );
 }
